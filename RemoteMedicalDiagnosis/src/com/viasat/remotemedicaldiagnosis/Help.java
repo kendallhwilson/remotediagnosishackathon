2ec5d264d7f6cc9a82113ea8feeac4fff89e3b44
kwilson <kendall.wilson@viasat.com>
2014-07-17 14:10:17 -0700

    deeed
    try

---
+++ RemoteMedicalDiagnosis/src/com/viasat/remotemedicaldiagnosis/Help.java
@@ -0,0 +1,13 @@
+package com.viasat.remotemedicaldiagnosis;
+
+import android.os.Bundle;
+
+public class Help extends NavigationDrawer
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) 
+    {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.help);
+    }
+}
