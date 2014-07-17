ce2165e673f1a4fc36e4e790c54fd81937bb30d1
Kaitlyn Taboada <Kaitlyn.Taboada@viasat.com>
2014-07-17 14:34:27 -0700

    pretty layout

--- RemoteMedicalDiagnosis/src/com/viasat/remotemedicaldiagnosis/BloodPressure.java
+++ RemoteMedicalDiagnosis/src/com/viasat/remotemedicaldiagnosis/BloodPressure.java
@@ -1,22 +1,37 @@
 package com.viasat.remotemedicaldiagnosis;
 
 import java.util.Random;
+
 import com.viasat.remotemedicaldiagnosis.R;
+
+
+
+
 
 
 
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnLayoutChangeListener;
+import android.view.ViewTreeObserver;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-public class BloodPressure extends NavigationDrawer
+public class BloodPressure extends NavigationDrawer implements OnGlobalLayoutListener
 {
 	
 	int systolic = 0;
 	int diastolic = 0;
 	int pulse = 0;
+	RelativeLayout bar;
+	ImageView redBlock;
+	ImageView darkOrangeBlock;
+	ImageView lightOrangeBlock;
+	ImageView yellowBlock;
+	ImageView greenBlock;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) 
@@ -24,9 +39,13 @@ public class BloodPressure extends NavigationDrawer
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.blood_pressure);
     	
-    	RelativeLayout bar = (RelativeLayout) findViewById(R.id.wellness_bar);
-    	ImageView redBlock = (ImageView) findViewById(R.id.red_block);
-    	redBlock.getLayoutParams().height = bar.getHeight()/5;
+    	bar = (RelativeLayout) findViewById(R.id.wellness_bar);
+    	redBlock = (ImageView) findViewById(R.id.red_block);
+    	darkOrangeBlock = (ImageView) findViewById(R.id.dark_orange_block);
+    	lightOrangeBlock = (ImageView) findViewById(R.id.light_orange_block);
+    	yellowBlock = (ImageView) findViewById(R.id.yellow_block);
+    	greenBlock = (ImageView) findViewById(R.id.green_block);
+    	
     	
     	Random random = new Random();
     	systolic = random.nextInt(100) + 100; //between 100-200
@@ -40,10 +59,24 @@ public class BloodPressure extends NavigationDrawer
     	}
     	else if (systolic > 140)
     	diastolic = random.nextInt(60) + 60;
-    	
-    	
+    	final ViewTreeObserver vto = bar.getViewTreeObserver();
+    	vto.addOnGlobalLayoutListener(this);
+
     }
+
+	@Override
+	public void onGlobalLayout() {
+		// TODO Auto-generated method stub
+		redBlock.getLayoutParams().height = bar.getHeight()/5 - 19;
+		darkOrangeBlock.getLayoutParams().height = bar.getHeight()/5 - 19;
+		lightOrangeBlock.getLayoutParams().height = bar.getHeight()/5 - 19;
+		yellowBlock.getLayoutParams().height = bar.getHeight()/5 - 19;
+		greenBlock.getLayoutParams().height = bar.getHeight()/5 - 19;
+		
+	}
     
-    
+  
+
+	
 	
 }
